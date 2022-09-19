47dd00988a9ee68509f76e638e0b2117564e3129
inbar2504 <113582785+inbar2504@users.noreply.github.com>
2022-09-19 16:03:51 +0300

    Update Screen.py

--- Screen.py
+++ Screen.py
@@ -13,8 +13,22 @@ finish = False
 x = 0
 y = 0
 
-#InitScreen
+def print_bush(field_matrix):
+    for i in range(25):
+        for j in range(50):
+            if bush_matrix[i][j] == "bush":
+                img.set_colorkey(consts.GREEN_BACKGROUND)
+                screen.blit(img, [i*30, j*15])
+    pygame.display.update()
 
+img = pygame.image.load(consts.IMAGE)
+field_matrix = MineField.matrix_construction()
+bush_matrix = MineField.fill_matrix(field_matrix, "bush", screen, img)
+full_matrix = MineField.fill_matrix(bush_matrix, "trap", screen, img)
+screen.fill(consts.GREEN_BACKGROUND)
+print_bush(bush_matrix)
+
+#InitScreen
 while not finish:
     screen.fill(consts.GREEN_BACKGROUND)
     keys = pygame.key.get_pressed()
@@ -28,12 +42,13 @@ while not finish:
         x -= 1
     screen.blit(solider_img, (x, y))
     MineField.Flag(screen)
-    pygame.display.flip()
+    print_bush(bush_matrix)
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             finish = True
     if event.type == pygame.KEYDOWN and (event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER):
-        print("tal")
         MineField.print_grid(screen)
 pygame.quit()
 
